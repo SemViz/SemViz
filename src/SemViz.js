29cf121f3a514cdaeaa7141e0ab8da272950eb19
simon louvet <simon.louvet.zen@gmail.com>
2018-03-09 16:53:48 +0100

    wip

--- src/SemViz.js
+++ src/SemViz.js
@@ -2,6 +2,15 @@ import '@webcomponents/webcomponentsjs';
 import qs from 'qs';
 let urlParamsString=window.location.search;
 var urlParams = qs.parse(urlParamsString,{ ignoreQueryPrefix: true });
+
+let importTemplateFile = document.createElement('link');
+importTemplateFile.rel='import';
+importTemplateFile.href="https://raw.githubusercontent.com/assemblee-virtuelle/SemViz/master/src/template.html";
+document.head.appendChild(importTemplateFile);
+let domBody = document.querySelectorAll('body');
+let domTemplate = document.createElement('semViz-base-template');
+domBody.appendChild(domComponent);
+
 console.log(urlParams.configFile);
 fetch(urlParams.configFile).then(function(response) {
   console.log(response);
