e7a472446f1bb4d28e5a1033b7b59af03b192ee5
simon louvet <simon.louvet.zen@gmail.com>
2018-03-09 16:16:05 +0100

    wip

---
+++ src/SemViz.js
@@ -0,0 +1,33 @@
+import '@webcomponents/webcomponentsjs';
+import qs from 'qs';
+let urlParamsString=window.location.search;
+var urlParams = qs.parse(urlParamsString,{ ignoreQueryPrefix: true });
+console.log(urlParams.configFile);
+fetch(urlParams.configFile).then(function(response) {
+  console.log(response);
+  return response.json();
+}).then(function(data) {
+  console.log(data);
+  for (let injection of data.injection) {
+    //console.log(injection);
+    let doms = document.querySelectorAll(injection.selector);
+    //console.log(doms);
+    for (let dom of doms) {
+      console.log(dom);
+      let importFile = document.createElement('link');
+      //importFile.setAttribut('rel','import');
+      // importFile.setAttribut('href',injection.componentUrl);
+      importFile.rel='import';
+      importFile.href=injection.componentUrl;
+      //let node = document.importNode(injection.componentUrl, true);
+      //dom.appendChild(importFile);
+      document.head.appendChild(importFile);
+
+      let domComponent = document.createElement(injection.componentTag);
+      dom.appendChild(domComponent);
+      //dom.innerHTML = injection.html
+    }
+  }
+}).catch(function(e) {
+  console.log("config Load Fail", e);
+});
