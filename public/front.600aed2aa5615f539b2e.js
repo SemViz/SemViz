8e34439093967f9efc9b1cf26a498ce1b153a093
simon louvet <simon.louvet.zen@gmail.com>
2018-03-21 15:15:57 +0100

    add error message if no configuration

--- public/front.600aed2aa5615f539b2e.js
+++ public/front.600aed2aa5615f539b2e.js
@@ -344,61 +344,64 @@ let urlParamsString = window.location.search;
 var urlParams = __WEBPACK_IMPORTED_MODULE_1_qs___default.a.parse(urlParamsString, {
   ignoreQueryPrefix: true
 });
+if (urlParams.configFile == undefined) {
+  document.querySelector('body').appendChild(document.createTextNode('SemViz have to be config by json config file (index.html?configFile=file.json)'));
+} else {
+  console.log(urlParams.configFile);
+  fetch(urlParams.configFile).then(function (response) {
+    console.log(response);
+    return response.json();
+  }).then(function (data) {
+    console.log(data);
 
-console.log(urlParams.configFile);
-fetch(urlParams.configFile).then(function (response) {
-  console.log(response);
-  return response.json();
-}).then(function (data) {
-  console.log(data);
+    window.customElements.whenDefined(data.template.componentTag).then(() => {
+      //console.log('ready!');
+      let Template = customElements.get(data.template.componentTag);
+      //console.log('elmt',Template);
+      let domTemplate = new Template(__WEBPACK_IMPORTED_MODULE_2_postal___default.a);
+      let domBody = document.querySelector('body');
+      domBody.appendChild(domTemplate);
 
-  window.customElements.whenDefined(data.template.componentTag).then(() => {
-    //console.log('ready!');
-    let Template = customElements.get(data.template.componentTag);
-    //console.log('elmt',Template);
-    let domTemplate = new Template(__WEBPACK_IMPORTED_MODULE_2_postal___default.a);
-    let domBody = document.querySelector('body');
-    domBody.appendChild(domTemplate);
-
-    for (let injection of data.injection) {
-      window.customElements.whenDefined(injection.componentTag).then(() => {
-        //console.log('ready!');
-        let Component = customElements.get(injection.componentTag);
-        let shadowDomTemplate = document.querySelector(data.template.componentTag);
-        //console.log(shadowDomTemplate);
-        let doms = shadowDomTemplate.shadowRoot.querySelectorAll(injection.selector);
-        console.log(doms);
-        for (let dom of doms) {
-          //console.log('elmt',Template);
-          let domComponent = new Component(__WEBPACK_IMPORTED_MODULE_2_postal___default.a);
-          dom.appendChild(domComponent);
-        }
-      });
-    }
-
-    let importSources = {};
-    for (let injection of data.injection) {
-      if (importSources[injection.componentTag] == undefined) {
-        let importFile = document.createElement('link');
-        importFile.rel = 'import';
-        importFile.href = injection.componentUrl;
-        document.head.appendChild(importFile);
-        importSources[injection.componentTag] = injection.componentUrl;
+      for (let injection of data.injection) {
+        window.customElements.whenDefined(injection.componentTag).then(() => {
+          //console.log('ready!');
+          let Component = customElements.get(injection.componentTag);
+          let shadowDomTemplate = document.querySelector(data.template.componentTag);
+          //console.log(shadowDomTemplate);
+          let doms = shadowDomTemplate.shadowRoot.querySelectorAll(injection.selector);
+          console.log(doms);
+          for (let dom of doms) {
+            //console.log('elmt',Template);
+            let domComponent = new Component(__WEBPACK_IMPORTED_MODULE_2_postal___default.a);
+            dom.appendChild(domComponent);
+          }
+        });
       }
-    }
+
+      let importSources = {};
+      for (let injection of data.injection) {
+        if (importSources[injection.componentTag] == undefined) {
+          let importFile = document.createElement('link');
+          importFile.rel = 'import';
+          importFile.href = injection.componentUrl;
+          document.head.appendChild(importFile);
+          importSources[injection.componentTag] = injection.componentUrl;
+        }
+      }
+    });
+
+    let importTemplateFile = document.createElement('link');
+    importTemplateFile.rel = 'import';
+    importTemplateFile.href = data.template.componentUrl;
+    //importTemplateFile.href="../src/template.html";
+    document.head.appendChild(importTemplateFile);
+
+    //console.log('SEMVIZ customElements',window.customElements);
+
+  }).catch(function (e) {
+    console.log("config Load Fail", e);
   });
-
-  let importTemplateFile = document.createElement('link');
-  importTemplateFile.rel = 'import';
-  importTemplateFile.href = data.template.componentUrl;
-  //importTemplateFile.href="../src/template.html";
-  document.head.appendChild(importTemplateFile);
-
-  //console.log('SEMVIZ customElements',window.customElements);
-
-}).catch(function (e) {
-  console.log("config Load Fail", e);
-});
+}
 
 /***/ }),
 /* 4 */
