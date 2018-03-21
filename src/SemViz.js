4c897aca768b17432b07ee82e2132ac8b7e7f5e3
simon louvet <simon.louvet.zen@gmail.com>
2018-03-21 15:12:11 +0100

    add error message if no configuration

--- src/SemViz.js
+++ src/SemViz.js
@@ -6,61 +6,62 @@ let urlParamsString = window.location.search;
 var urlParams = qs.parse(urlParamsString, {
   ignoreQueryPrefix: true
 });
+if(urlParams.configFile==undefined){
+  document.querySelector('body').appendChild(document.createTextNode('SemViz have to be config by json config file (index.html?configFile=file.json)'))
+}else{
+  console.log(urlParams.configFile);
+  fetch(urlParams.configFile).then(function(response) {
+    console.log(response);
+    return response.json();
+  }).then(function(data) {
+    console.log(data);
+
+    window.customElements.whenDefined(data.template.componentTag).then(() => {
+      //console.log('ready!');
+      let Template = customElements.get(data.template.componentTag);
+      //console.log('elmt',Template);
+      let domTemplate = new Template(postal);
+      let domBody = document.querySelector('body');
+      domBody.appendChild(domTemplate);
+
+      for (let injection of data.injection) {
+        window.customElements.whenDefined(injection.componentTag).then(() => {
+          //console.log('ready!');
+          let Component = customElements.get(injection.componentTag);
+          let shadowDomTemplate = document.querySelector(data.template.componentTag);
+          //console.log(shadowDomTemplate);
+          let doms = shadowDomTemplate.shadowRoot.querySelectorAll(injection.selector);
+          console.log(doms);
+          for (let dom of doms) {
+            //console.log('elmt',Template);
+            let domComponent = new Component(postal);
+            dom.appendChild(domComponent);
+          }
+        });
+      }
+
+      let importSources={};
+      for (let injection of data.injection) {
+          if(importSources[injection.componentTag]==undefined){
+            let importFile = document.createElement('link');
+            importFile.rel = 'import';
+            importFile.href = injection.componentUrl;
+            document.head.appendChild(importFile);
+            importSources[injection.componentTag]=injection.componentUrl;
+          }
+      }
+    });
+
+    let importTemplateFile = document.createElement('link');
+    importTemplateFile.rel = 'import';
+    importTemplateFile.href = data.template.componentUrl;
+    //importTemplateFile.href="../src/template.html";
+    document.head.appendChild(importTemplateFile);
+
+    //console.log('SEMVIZ customElements',window.customElements);
 
 
-
-console.log(urlParams.configFile);
-fetch(urlParams.configFile).then(function(response) {
-  console.log(response);
-  return response.json();
-}).then(function(data) {
-  console.log(data);
-
-  window.customElements.whenDefined(data.template.componentTag).then(() => {
-    //console.log('ready!');
-    let Template = customElements.get(data.template.componentTag);
-    //console.log('elmt',Template);
-    let domTemplate = new Template(postal);
-    let domBody = document.querySelector('body');
-    domBody.appendChild(domTemplate);
-
-    for (let injection of data.injection) {
-      window.customElements.whenDefined(injection.componentTag).then(() => {
-        //console.log('ready!');
-        let Component = customElements.get(injection.componentTag);
-        let shadowDomTemplate = document.querySelector(data.template.componentTag);
-        //console.log(shadowDomTemplate);
-        let doms = shadowDomTemplate.shadowRoot.querySelectorAll(injection.selector);
-        console.log(doms);
-        for (let dom of doms) {
-          //console.log('elmt',Template);
-          let domComponent = new Component(postal);
-          dom.appendChild(domComponent);
-        }
-      });
-    }
-
-    let importSources={};
-    for (let injection of data.injection) {
-        if(importSources[injection.componentTag]==undefined){
-          let importFile = document.createElement('link');
-          importFile.rel = 'import';
-          importFile.href = injection.componentUrl;
-          document.head.appendChild(importFile);
-          importSources[injection.componentTag]=injection.componentUrl;
-        }
-    }
+  }).catch(function(e) {
+    console.log("config Load Fail", e);
   });
-
-  let importTemplateFile = document.createElement('link');
-  importTemplateFile.rel = 'import';
-  importTemplateFile.href = data.template.componentUrl;
-  //importTemplateFile.href="../src/template.html";
-  document.head.appendChild(importTemplateFile);
-
-  //console.log('SEMVIZ customElements',window.customElements);
-
-
-}).catch(function(e) {
-  console.log("config Load Fail", e);
-});
+}
